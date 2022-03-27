93bff1f4668c7b455dd4c386d26e20aff701f233
Abdelrahman Ibrahim <abdelramin30@gmail.com>
2022-03-27 03:17:58 +0200

    Add Navbar part 1

--- js/script.js
+++ js/script.js
@@ -25,3 +25,23 @@ a.onclick = function(){
 
 };
 new WOW().init();
+
+function showbigFunc(){
+    let navsmall=document.querySelector(".small-nav");
+    let navbig=document.querySelector(".big-nav");
+    let navMobileSmall=document.querySelector(".small-mobile-nav");
+    let navMobileBig=document.querySelector(".big-mobile-nav");
+    navsmall.classList.add("hideClass");
+    navsmall.classList.remove("showClass");
+    navbig.classList.add("showClass");
+    navbig.classList.remove("hideClass");
+// Mobile
+    navMobileSmall.classList.add("hideClass");
+    navMobileSmall.classList.remove("showClass");
+    
+    navMobileBig.classList.add("showClass");
+    navMobileBig.classList.remove("hideClass");
+    
+}
+// let nav=document.querySelector("#small-nav");
+nav.className="big-nav";
