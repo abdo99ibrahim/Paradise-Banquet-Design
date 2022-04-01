3766deab03ecbf2d945d9de37a99c98b000e350b
Abdelrahman Ibrahim <abdelramin30@gmail.com>
2022-04-01 17:37:26 +0200

    changes

--- js/script.js
+++ js/script.js
@@ -71,3 +71,16 @@ function hide(){
     navMobileBig.classList.remove("showClass");
     navMobileBig.classList.add("hideClass");
 }
+
+$('.slider').on('initialized.owl.carousel changed.owl.carousel', function(e) {
+    if (!e.namespace)  {
+      return;
+    }
+    var carousel = e.relatedTarget;
+    $('.slider-counter').text(carousel.relative(carousel.current()) + 1 + '/' + carousel.items().length);
+  }).owlCarousel({
+    items: 1,
+    loop:true,
+    margin:0,
+    nav:true,
+  });
